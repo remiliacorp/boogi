a578e6d0f71d26080a4137ff4ae4a97fcdd0f5fb
remiliacorp <80489490+remiliacorp@users.noreply.github.com>
2021-03-21 22:27:25 -0700

    Update light.js

--- src/theme/light.js
+++ src/theme/light.js
@@ -3,14 +3,14 @@ import defaultColors from './colors';
 const colors = {
   ...defaultColors,
 
-  primary: defaultColors.black,
+  primary: '#008000',
   primaryDark: defaultColors.red,
   font: '#000000',
   fontDark: '#008000',
   background: '#ffffe0',
   mainBackground: '#ffffe0',
   border: '#000000',
-  hover: defaultColors.black,
+  hover: '#008000',
   shadow: defaultColors.black + '33',
 };
 
